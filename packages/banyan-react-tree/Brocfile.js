50f676962c3d6e30741db9ef4b97668c7177fb15
Marco Braak <mbraak@ridethepony.nl>
2015-09-11 11:49:44 +0200

    Refactored broccoli file

--- packages/banyan-react-tree/Brocfile.js
+++ packages/banyan-react-tree/Brocfile.js
@@ -6,41 +6,61 @@ var compileCSS = require('broccoli-postcss');
 var Funnel = require('broccoli-funnel');
 
 
-var lib_tree = pickFiles('src', {
-	files: ['**/*.js'],
-	srcDir: '.',
-	destDir: './build'
-});
+function createSourceTree() {
+	return pickFiles('src', {
+		files: ['**/*.js'],
+		srcDir: '.',
+		destDir: './build'
+	});
+}
 
-var babel_tree = babelTranspiler(
-    lib_tree, {
-        browserPolyfill: true,
-        stage: 0
-    });
+function createBabelTree(source_tree) {
+	return babelTranspiler(
+	    source_tree,
+		{
+	        browserPolyfill: true,
+	        stage: 0
+	    }
+	);
+}
 
-var browserify_tree = fastBrowserify(
-    babel_tree, {
-        bundles: {
-    		'example.js': {
-    			entryPoints: ['./build/examples/example.js']
-    		}
-    	}
-    }
-);
+function createBrowserifyTree(babel_tree) {
+	return fastBrowserify(
+	    babel_tree, {
+	        bundles: {
+	    		'example.js': {
+	    			entryPoints: ['./build/examples/example.js']
+	    		}
+	    	}
+	    }
+	);
+}
 
-var css_tree = compileCSS(
-    ['css'],
-    'banyan-react-tree.css',
-    'banyan-react-tree.css',
-    [
-        {module: require('postcss-nested')}
-    ]
-);
+function createCssTree() {
+	return compileCSS(
+	    ['css'],
+	    'banyan-react-tree.css',
+	    'banyan-react-tree.css',
+	    [
+	        {module: require('postcss-nested')}
+	    ]
+	);
+}
 
-var example_tree = new Funnel(
-    'examples', {
-        destDir: './'
-    }
-);
+function createExampletree() {
+	return new Funnel(
+	    'examples', {
+	        destDir: './'
+	    }
+	);
+}
 
-module.exports = mergeTrees([browserify_tree, css_tree, example_tree]);
+module.exports = mergeTrees([
+	createBrowserifyTree(
+		createBabelTree(
+			createSourceTree()
+		)
+	),
+	createCssTree(),
+	createExampletree()
+]);
