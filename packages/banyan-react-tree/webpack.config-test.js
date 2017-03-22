7b7ec1781b2d2a995c5fab68ac0eb00a8b796539
Marco Braak <mbraak@ridethepony.nl>
2017-03-22 09:10:47 +0100

    Add coverage

--- packages/banyan-react-tree/webpack.config-test.js
+++ packages/banyan-react-tree/webpack.config-test.js
@@ -1,18 +1,39 @@
+var path = require('path');
 var nodeExternals = require('webpack-node-externals');
- 
+
+var isCoverage = process.env.NODE_ENV === 'coverage';
+
+var loaders = [];
+
+if (isCoverage) {
+    loaders.push({
+        test: /\.(js|tsx?)/,
+        include: path.resolve('src'),
+        loader: 'istanbul-instrumenter-loader',
+        query: {
+          esModules: true
+        }
+    })
+}
+
+loaders.push({
+    test: /\.tsx?$/,
+    loader: "ts-loader",
+    exclude: /node_modules/    
+});
+
 module.exports = {
     target: "node",
     externals: [nodeExternals()],
+    devtool: "inline-cheap-module-source-map",
     resolve: {
         extensions: [".ts", ".tsx", ".js"]
     },
+    output: {
+        devtoolModuleFilenameTemplate: '[absolute-resource-path]',
+        devtoolFallbackModuleFilenameTemplate: '[absolute-resource-path]?[hash]'
+    },
     module: {
-        loaders: [
-            {
-                test: /\.tsx?$/,
-                loader: "ts-loader",
-                exclude: /node_modules/
-            }
-        ]
+        loaders: loaders
     }
 }
