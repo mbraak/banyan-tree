11e10898d05f9700fd64c561986989335fb68732
Marco Braak <marco.braak@planet.nl>
2017-05-24 13:13:19 +0200

    Use dinosaur-data package

--- packages/banyan-react-tree/examples/default/example.tsx
+++ packages/banyan-react-tree/examples/default/example.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Tree } from "banyan-immutable-tree/lib/immutable_tree";
+import { example_data } from "dinosaur-data";
 
 import App from "./app";
-import example_data from "../dinosaurs.json";
 
 import "../../css/banyan-react-tree.css";
 import "../example.css";
 
-const tree = new Tree([example_data])
+const tree = new Tree([ example_data ])
     .openLevel(1);
 
 ReactDOM.render(
