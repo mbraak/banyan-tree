9d8598e962a788ecbc80609f7fae2f47c8c11b30
Marco Braak <marco@invoicefinance.nl>
2017-10-23 17:38:37 +0200

    Remove babel

--- packages/banyan-mobx-tree/gulpfile.js
+++ packages/banyan-mobx-tree/gulpfile.js
@@ -1,6 +1,5 @@
 const gulp = require("gulp");
 const ts = require("gulp-typescript");
-const babel = require("gulp-babel");
 const sourcemaps = require("gulp-sourcemaps");
 
 gulp.task("lib_ts", function() {
@@ -11,10 +10,7 @@ gulp.task("lib_ts", function() {
         .pipe(sourcemaps.init())
         .pipe(tsProject());
 
-    js
-        .pipe(babel({ presets: "env" }))
-        .pipe(sourcemaps.write("./"))
-        .pipe(gulp.dest("./lib"));
+    js.pipe(sourcemaps.write("./")).pipe(gulp.dest("./lib"));
 
     dts.pipe(gulp.dest("./lib"));
 });
