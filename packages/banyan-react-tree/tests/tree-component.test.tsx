3adf59c06c4718a7cbc9cc30cca5cf955e5258b6
Marco Braak <marco@invoicefinance.nl>
2017-11-02 12:59:36 +0100

    Restore tests

---
+++ packages/banyan-react-tree/tests/tree-component.test.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { mount, configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+import { Tree } from "banyan-immutable-tree/lib/immutable_tree";
+
+import { test_data, example_data } from "./testutil/example_data";
+import { render, treeElementToString } from "./testutil/render";
+
+import { TreeComponent } from "../src/tree_component";
+
+configure({ adapter: new Adapter() });
+
+test("render", () => {
+    const tree = new Tree(test_data).openAllFolders();
+
+    const el = render(<TreeComponent tree={tree} />);
+    expect(el.hasClass("banyan-tree")).toBe(true);
+
+    expect(treeElementToString(el)).toBe("n1(n1a n1b) n2(n2a n2b)");
+});
+
+test("select", () => {
+    // setup
+    const original_tree = new Tree(example_data).openAllFolders();
+
+    // find titles
+    const wrapper1 = mount(<TreeComponent tree={original_tree} />);
+
+    const currentTree = () => wrapper1.state("tree") as Tree;
+
+    expect(wrapper1.find(".banyan-title").length).toBe(31);
+    expect(wrapper1.find(".banyan-selected").length).toBe(0);
+
+    expect(currentTree().getSelectedNode()).toBe(null);
+
+    // click on node
+    const div = wrapper1
+        .findWhere(
+            el =>
+                el.name() === "TreeNode" &&
+                el.prop("node").get("name") === "Theropods"
+        )
+        .first()
+        .find("li div")
+        .first();
+
+    div.simulate("click");
+
+    const selected_node = currentTree().getSelectedNode();
+    expect(selected_node).not.toBe(null);
+
+    if (selected_node) {
+        expect(selected_node.get("name")).toBe("Theropods");
+        expect(selected_node.get("is_selected")).toBe(true);
+    }
+
+    // check original tree
+    // expect(original_tree.getSelectedNode()).toBeNull();  // todo: fix in immutable-tree
+    expect(
+        original_tree.getNodeByName("Theropods")!.get("is_selected")
+    ).toBeFalsy();
+
+    // create new component from tree
+    const wrapper2 = mount(<TreeComponent tree={currentTree()} />);
+    expect(wrapper2.find(".banyan-selected").length).toBe(1);
+});
